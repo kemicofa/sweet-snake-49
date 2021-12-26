7c3993deba626fd31e8bc6eb0ce794e53219b49b
Kevin Faulhaber <kevin.faulhaber@gmail.com>
2021-12-26 19:38:33 +0100

    fix: workaround to oak#send since not implemented in DD

--- back/main.ts
+++ back/main.ts
@@ -1,6 +1,7 @@
 import { Application, Router, send } from "./deps.ts";
 import { activeGamesCount, clearInactiveGames, createSnakeGame, updateSnakeGame } from "./game.ts";
 import { getHighscores } from './highscore.ts';
+import mime from 'https://cdn.skypack.dev/mime-types'
 
 const app = new Application();
 
@@ -29,10 +30,19 @@ app.use(router.routes());
 app.use(router.allowedMethods());
 
 app.use(async (context) => {
-  await send(context, context.request.url.pathname, {
-    root: `${Deno.cwd()}/public`,
-    index: "index.html",
-  });
+  // TODO: send does not work on Deno Deploy, must wait till Deno.stat is implemented on DD.
+  // await send(context, context.request.url.pathname, {
+  //   root: `${Deno.cwd()}/public`,
+  //   index: "index.html",
+  // });
+  const pathname = context.request.url.pathname === '/' ? '/index.html' : context.request.url.pathname
+  const requestMimeType = mime.lookup(pathname)
+  const responseMimeType = mime.contentType(requestMimeType)
+  context.response.headers.set('Content-Type', responseMimeType);
+  const location = `${Deno.cwd()}/public` + pathname;
+  context.response.body = requestMimeType === 'image/png' ? 
+    await Deno.readFile(location): 
+    await Deno.readTextFile(location);
 });
 
 await app.listen({ port: 8000 });
